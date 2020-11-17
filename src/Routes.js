223e9621832af2622c77fed4602c67aa4b38f0ae
hyesoo-ahn <hyesoo-ahn@users.noreply.github.com>
2020-11-17 11:08:39 +0900

    [add] login page

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React, {createRef} from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Cart from './pages/Cart/Cart';
+
+class Routes extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/Login' component={Login} />
+                    <Route exact path='/SignUp' component={SignUp} />
+                    <Route exact path='/Cart' component={Cart} />
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default Routes;
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './Login.scss';
+// import '../../styles/reset.scss';
+
+class Login extends Component {
+    render() {
+        return (
+        <div className="loginContainer">
+            <div class="logo">
+                <img class='logoImage' src='./images/logo.png' />
+            </div>  
+            <div className="messageBox">
+                <img className="gmarketLogo" src="https://pics.gmarket.co.kr/pc/single/kr/common/image__logo.png" alt="gmarketLogo" />
+                <span class='gmarketMessage'>아이디로 이용이 가능합니다.</span>
+            </div>
+            <form>
+                <div className = 'inputBox'>
+                    <input className="inputId" placeholder="아이디" />
+                    <input className="inputPw" placeholder="비밀번호" />
+                </div>
+                <div class='saveId'>
+                    <input type="checkbox" id="cb1" />
+                    <label for="cb1" />
+                    <div className="rememberId">아이디 기억하기</div>
+                </div>
+                <div className='loginWrap'> 
+                    <button className="loginButton">로그인</button>
+                    <button className="kakaoLoginButton">
+                        <img className='kakaoImg' src='./images/kakao.png' />
+                        카카오 로그인
+                    </button>
+                </div>
+                <div className = "linkGroup">
+                    <span className="signUp">회원가입</span>
+                    <span className="gap">|</span>
+                    <span className="findId">아이디 찾기</span>
+                    <span className="gap">|</span>
+                    <span className="findPw">비밀번호 찾기</span>
+                </div>
+                <div className="nonMember">
+                    <div className="nonMemberOrder">비회원 주문 조회</div>
+                    <input type='text' className='orderName' placeholder="주문자 이름" />
+                    <input type='text' className='phoneNumber' placeholder="휴대폰 번호" />
+                    <input type='text' className='orderPw' placeholder="주문 비밀번호" />
+                    <div className="orderMessageBox">
+                    주문 시 입력한 주문자 명과 휴대폰 번호, 주문 비밀번호를 정확히 입력해주세요.
+                    </div>
+                    <button className="nonMemberOrderHistory">비회원 주문내역 조회</button>
+                </div>
+            </form>
+
+            
+        </div>
+        );
+    }
+}
+
+export default Login;
