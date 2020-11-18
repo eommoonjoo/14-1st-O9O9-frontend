8b0af440538cfa4d52cca94d695b3616d7851411
hyesoo-ahn <hyesoo-ahn@users.noreply.github.com>
2020-11-18 13:19:03 +0900

    초기세팅

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Cart.scss';
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+               <div className='nav'>
+                   <img className='logo' src=''></img>
+               </div>
+            </div>
+        );
+    }
+}
+
+export default Cart;
