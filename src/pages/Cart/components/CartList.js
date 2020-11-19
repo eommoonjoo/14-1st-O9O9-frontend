e6d55ce4fe55781bd26c249db46ec0aa3edb7f2e
hyesoo-ahn <hyesoo-ahn@users.noreply.github.com>
2020-11-19 16:08:08 +0900

    [add]mock데이터 추가

--- src/pages/Cart/components/CartList.js
+++ src/pages/Cart/components/CartList.js
@@ -1,36 +1,65 @@
 import React, { Component } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 
-
+const API = 'http://localhost:3000/data/cartdata.json'
+ 
 class CartList extends Component {
+    constructor() {
+        super();
+        this.state = {
+           cartItem: []
+        };
+    }
+
+    componentDidMount = () => {
+      fetch(API, {
+       method: 'GET'
+      })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          cartItem: res.cartItems
+        });
+      });
+    }
+
     render() {
+        const {cartItem} = this.state;
         return (
-            <div className='selectionList'>
-                <div className='productName'>
-                    <div className='productTitle'>
-                        <input type="checkbox" id="checkBox" />
-                        <label for="checkBox" />
-                        <div className='productId'>서귀포 조생 감귤 </div>
-                    </div>
-                    <AiOutlineClose size="30" className='XIcon'/>
-                </div>
-                <div className='product'>
-                    <img className='productImage' src='https://image.g9.co.kr/g/1893145348/8' alt="이미지"/>
-                    <div className='priceList'>
-                        <div className='price'><span>10900</span><span className='won'>원</span></div>
-                        <div className='amount'>
-                            <table>
-                                <tr>
-                                <td className='minus'>-</td>
-                                <td>수량</td>
-                                <td className='plus'>+</td>
-                                </tr>
-                            </table>
-                            <button className='buy'>즉시구매</button>
+            <>
+            {cartItem &&  cartItem.map((el) => {
+                return (
+                    <div className='selectionList'>
+                        <div className='productName'>
+                        <div className='productTitle'>
+                            <input type="checkbox" id="checkBox" />
+                            <label for="checkBox" />
+                            <div className='productId'>{el.productName} </div>
                         </div>
+                        <AiOutlineClose size="30" className='XIcon'/>
+                    </div>
+                    <div className='product'>
+                         <img className='productImage' src={el.url} alt="이미지"/>
+                        <div className='priceList'>
+                            <div className='price'><span>{el.price}</span>
+                            <span className='won'>원</span>
+                        </div>
+                        <div className='amount'>
+                        <table>
+                            <tr>
+                            <td className='minus'>-</td>
+                            <td>수량</td>
+                            <td className='plus'>+</td>
+                            </tr>
+                        </table>
+                        <button className='buy'>즉시구매</button>
                     </div>
                 </div>
             </div>
+        </div>
+            )
+          })}
+        </>
         );
     }
 }
