66c88e80ece9a77c7561cfc845d275999792a799
hyesoo-ahn <hyesoo-ahn@users.noreply.github.com>
2020-11-20 21:59:39 +0900

    refactoring

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -73,83 +73,83 @@ class SignUp extends Component {
                     <div className='joinText'>잠깐 G마켓 회원이신가요?</div>
                     <div className='joinTextBox'>
                         <span className='joinText'>G9는 </span>
-                        <img className = 'gmarketImage' src='./images/gmarket.png' />
+                            <img src='./images/gmarket.png' />
                         <span className='joinText'>아이디로 이용이 가능합니다. </span>
                         <span className='login'>로그인하기 {'>'}</span>
                     </div>
                 
                     <div className='idAndPw'>
                         <div className='idSection'>
-                            <input className='idInput' placeholder='아이디' name="id" onChange={this.handleInputValueChange}/>
-                            <button className='checkDuplication' onClick={this.handleDuplication}>중복확인</button>
+                            <input placeholder='아이디' name="id" onChange={this.handleInputValueChange}/>
+                            <button onClick={this.handleDuplication}>중복확인</button>
                         </div>
                         <div className='pwSection'>
-                            <input className='pwInput' type='password' placeholder='비밀번호' name="pw" onChange={this.handleInputValueChange}/>
+                            <input type='password' placeholder='비밀번호' name="pw" onChange={this.handleInputValueChange}/>
                         </div>
                         <div className='pwAgainSection'>
-                            <input className='pwInput' type='password' name="passwordcheck" onChange={this.handleInputValueChange} placeholder='비밀번호 확인'/>
+                            <input type='password' name="passwordcheck" onChange={this.handleInputValueChange} placeholder='비밀번호 확인'/>
                         </div>
                         <div className='userInfo'>
                             <input className='nameInput' placeholder='이름' name="name" onChange={this.handleInputValueChange} />
                             <input className='phoneNumberInput' placeholder='휴대폰 번호' name="phone" onChange={this.handleInputValueChange} />
                             <input className='emailInput' placeholder='이메일' name="email" onChange={this.handleInputValueChange} />
-                            <div className='infoText'> ※ 만 14세 이상 고객만 가입이 가능합니다.</div>
+                            <div> ※ 만 14세 이상 고객만 가입이 가능합니다.</div>
                         </div>
                     </div>
                     
                     <div className='checkBox'>
                         <div className='agreement'>
-                            <input type="checkbox" id="cb1" />
+                            <input type="checkbox"/>
                             <label for="cb1" />
-                            <div className='agreementAll'>전체동의</div>
+                            <div>전체동의</div>
                         </div>
                         <div className='agreementList'>
                             <div className='checkAll'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox"/>
                                 <label for="cb1" />
-                                <div className='agreementAll'>필수항목 전체동의</div>
+                                <div>필수항목 전체동의</div>
                             </div>
                             <div className='check'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox"/>
                                 <label for="cb1" />
-                                <div className='agreementAll'>G9 구매회원 약관</div>
+                                <div>G9 구매회원 약관</div>
                             </div>
                             <div className='check'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox"/>
                                 <label for="cb1" />
-                                <div className='agreementAll'>G마켓 구매회원 약관</div>
+                                <div>G마켓 구매회원 약관</div>
                             </div>
                             <div className='check'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox"/>
                                 <label for="cb1" />
-                                <div className='agreementAll'>개인정보 수집 및 이용</div>
+                                <div>개인정보 수집 및 이용</div>
                             </div>
                             <div className='line'></div>
                         </div>
                         <div className='optionalAgreementList'>
                             <div className='checkAll'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox" />
                                 <label for="cb1" />
-                                <div className='agreementAll'>선택항목 전체동의</div>
+                                <div>선택항목 전체동의</div>
                             </div>
                             <div className='check'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox" />
                                 <label for="cb1" />
-                                <div className='agreementAll'>개인정보의 제3자 제공</div>
+                                <div>개인정보의 제3자 제공</div>
                             </div>
                             <div className='check'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox" />
                                 <label for="cb1" />
-                                <div className='agreementAll'>개인정보 수집 및 이용</div>
+                                <div>개인정보 수집 및 이용</div>
                             </div>
                             <div className='check'>
-                                <input type="checkbox" id="cb1" />
+                                <input type="checkbox" />
                                 <label for="cb1" />
-                                <div className='agreementAll'>쇼핑 이메일 수신</div>
+                                <div>쇼핑 이메일 수신</div>
                             </div>
                             <div className='marketingInfo'>상품구매 관련 내용은 수신동의 여부와 관계없이 발송됩니다.</div>
                         </div>
-                        <button className='signUpButton' onClick={this.checkValidation}>동의하고 회원가입</button>
+                        <button onClick={this.checkValidation}>동의하고 회원가입</button>
                     </div>
                 </div>
             </>
