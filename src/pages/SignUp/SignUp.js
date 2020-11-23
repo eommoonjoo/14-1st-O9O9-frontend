cf60d257a9f77eee73635fcb86040374a52b7235
hyesoo-ahn <hyesoo-ahn@users.noreply.github.com>
2020-11-23 13:57:39 +0900

    리뷰 반영&추가 기능 구현

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -10,12 +10,39 @@ class SignUp extends Component {
             name: '',
             phone:'',
             email: '',
-            passwordcheck: ''
+            passwordcheck: '',
+
+            validID: true,
+            validPW: true,
+            validDoubleCheck: true,
+            validName: true,
+            validPhone: true,
+            validEmail: true,
+        }
+    }
+    validationCheck = (type, value) => {
+        if (type==='id') {
+            let idReg = /^[a-z]+[a-z0-9]{5,19}$/g;
+            const validID = idReg.test(value);
+            this.setState({validID});
+        } else if (type==='pw') {
+            let passwordRules = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+            const validPW = passwordRules.test(value);
+            this.setState({validPW})
+        } else if (type === 'name') {
+            let reg = /^[가-힣]{2,4}$/;
+            const validName = reg.test(value);
+            this.setState({validName});
+        } else if (type === 'email') {
+            let emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+            const validEmail = emailRule.test(value);
+            this.setState({validEmail})
         }
     }
 
     handleInputValueChange = (e) => {
         // const { id, pw, name, phone, email } = e.target;
+        this.validationCheck(e.target.name, e.target.value);
         this.setState({[e.target.name]: e.target.value});
     }
 
@@ -65,10 +92,10 @@ class SignUp extends Component {
     render() {
         // console.log(this.state)
         return (
-            <>
+            <div className='SignUp'>
                 <div className='joinContent'>
                     <div className='logo'>
-                        <img className='o9Logo' src='./images/logo_transparent.png'/>
+                        <img src='./images/logo_transparent.png'/>
                     </div>
                     <div className='joinText'>잠깐 G마켓 회원이신가요?</div>
                     <div className='joinTextBox'>
@@ -82,18 +109,31 @@ class SignUp extends Component {
                         <div className='idSection'>
                             <input placeholder='아이디' name="id" onChange={this.handleInputValueChange}/>
                             <button onClick={this.handleDuplication}>중복확인</button>
+                            {!this.state.validID && <p>회원아이디를 정확히 입력해 주세요.</p>}
                         </div>
                         <div className='pwSection'>
                             <input type='password' placeholder='비밀번호' name="pw" onChange={this.handleInputValueChange}/>
+                            {!this.state.validPW && <p>비밀번호는 영문 대/소문자, 숫자 및 특수문자 2가지 이상의 조합으로 입력해야 합니다.</p>}
+                            
                         </div>
                         <div className='pwAgainSection'>
                             <input type='password' name="passwordcheck" onChange={this.handleInputValueChange} placeholder='비밀번호 확인'/>
+                            <p>비밀번호가 일치하지 않습니다.</p>
                         </div>
                         <div className='userInfo'>
-                            <input className='nameInput' placeholder='이름' name="name" onChange={this.handleInputValueChange} />
-                            <input className='phoneNumberInput' placeholder='휴대폰 번호' name="phone" onChange={this.handleInputValueChange} />
-                            <input className='emailInput' placeholder='이메일' name="email" onChange={this.handleInputValueChange} />
-                            <div> ※ 만 14세 이상 고객만 가입이 가능합니다.</div>
+                            <div>
+                                <input className='nameInput' placeholder='이름' name="name" onChange={this.handleInputValueChange} />
+                                {!this.state.validName && <p>이름을 입력해 주세요.</p>}
+                            </div>
+                            <div>
+                                <input className='phoneNumberInput' placeholder='휴대폰 번호' name="phone" onChange={this.handleInputValueChange} />
+                                {!this.state.validPhone && <p>휴대폰 번호를 정확히 입력해 주세요.</p>}
+                            </div>
+                            <div>
+                                <input className='emailInput' placeholder='이메일' name="email" onChange={this.handleInputValueChange} />
+                                {!this.state.validEmail && <p>메일주소를 정확히 입력해 주세요.</p>}
+                            </div>
+                            <div className='textSignUp'> ※ 만 14세 이상 고객만 가입이 가능합니다.</div>
                         </div>
                     </div>
                     
@@ -152,7 +192,7 @@ class SignUp extends Component {
                         <button onClick={this.checkValidation}>동의하고 회원가입</button>
                     </div>
                 </div>
-            </>
+            </div>
         );
     }
 }
