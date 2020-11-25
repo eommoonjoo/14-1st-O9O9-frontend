2021c7aca3bf42651451db0d3ed0d4a66623411a
hyesoo-ahn <hyesoo-ahn@users.noreply.github.com>
2020-11-25 20:55:23 +0900

    nothing changed

--- src/config.js
+++ src/config.js
@@ -6,4 +6,3 @@ export const PRODUCTS_MOCK_DATA = "http://localhost:3000/data/productsdata.json"
 export const MAINCATEGORY_PRODUCTS_DATA_API = "http://172.30.1.29:8000/products/categories";
 export const SUBCATEGORY_PRODUCTS_DATA_API = "";
 
-
